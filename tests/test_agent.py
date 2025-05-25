4357360aeb2181dc7fd60af7fbdf1f6c50e0d473
adwaye <adwayerambojun@gmail.com>
2025-05-25 21:23:21 +0100

    feat: added dqn skeleton

---
+++ tests/test_agent.py
@@ -0,0 +1,22 @@
+
+from __future__ import annotations
+from unittest import TestCase
+
+import random
+import numpy as np
+from games_agent.agent import DQN
+from parameterized import parameterized
+
+def TestDQN(TestCase):
+    def setUp(self):
+        self.n_actions = 4  # Example number of actions
+        self.model = DQN(self.n_actions)
+
+    
+    def test_forward_shape(self):
+        input_tensor = torch.tensor(np.random.rand(1, 1, 4, 4), dtype=torch.float32)
+        output = self.model(input_tensor)
+        self.assertEqual(output.shape[0], 2)  # Batch size
+        self.assertEqual(output.shape[2], 2)
+        # self.assertEqual(output.shape[1], self.n_actions)  # Number of actions
+        assert output.shape[0] ==2
