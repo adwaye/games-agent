8b9b1ef5a0d7a88fd1027c47343aea048cca053a
adwaye <adwayerambojun@gmail.com>
2025-05-31 22:55:21 +0100

    feat: added automatic dir finding for launch script

--- scripts/launch_training_script.sh
+++ scripts/launch_training_script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 python \
-/home/adwaye/AI_Models/games_agent/src/games_agent/scripts/run_train.py \
--cp /home/adwaye/AI_Models/games_agent/src/games_agent/configs/ \
+$SCRIPT_DIR'/src/games_agent/scripts/run_train.py' \
+-cp $SCRIPT_DIR'src/games_agent/configs/' \
 -m agent.env.reward_function='merge_only,'full_merge','log_full_merge' \
 agent.env.reward_scaling='total_value','total_steps,'current'
